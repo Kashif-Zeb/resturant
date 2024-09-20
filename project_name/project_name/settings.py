5730b7cf72be0ce33579c7d2cf52564da3086b58
Kashif-Zeb <128372577+Kashif-Zeb@users.noreply.github.com>
2024-09-20 14:39:40 +0500

    Update settings.py

--- project_name/project_name/settings.py
+++ project_name/project_name/settings.py
@@ -160,7 +160,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST =  'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'kashifzeb19@gmail.com'
-EMAIL_HOST_PASSWORD = 'tqoy bzen jlxl kgyn'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 
 # settings.py
